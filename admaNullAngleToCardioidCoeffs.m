9dde285057868fb4367683fb4aadc67410ad4bdf
Thomas Fehér <thomas.feher@yahoo.de>
2013-06-12 16:27:45 +0200

    admaCardioidCoeffToPattern: add tests, fix bug in documentation

--- admaNullAngleToCardioidCoeffs.m
+++ admaNullAngleToCardioidCoeffs.m
@@ -4,7 +4,16 @@
 	%angle: angle of the pattern null in degree
 %output:
 	%weight: weight a to calculate the resulting pattern as follows:
-	% 		weight*front_cardioid + (1-weight)*back_cardioid = result
+	% 		weight*front_cardioid - (1-weight)*back_cardioid = result
 function weight = admaNullAngleToCardioidCoeffs(angle)
 angle = angle/180*pi;
 weight = (1-cos(angle))/2;
+
+%!test
+%! assert(eps,0.5,admaNullAngleToCardioidCoeffs(90))
+
+%!test
+%! assert(eps,1/3,admaNullAngleToCardioidCoeffs(120))
+
+%!test
+%! assert(eps,0,admaNullAngleToCardioidCoeffs(180))
