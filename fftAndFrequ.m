185e9640ea53d587e71c1a81ab8e370fa6e810de
Thomas Fehér <thomasfeher@web.de>
2014-03-30 17:21:34 +0200

    fix bug
    Frequency calculation for odd number of samples was incorrect.

--- fftAndFrequ.m
+++ fftAndFrequ.m
@@ -13,9 +13,19 @@ end
 
 sigFd = fft(sig.').';
 fftSize = size(sigFd,2);
-frequ = fs/2 * linspace(0,1,fftSize/2+1);
-if(mod(fftSize,2))%odd fft size (fs/2 is omitted)
-	frequ = [frequ(1:end-1) frequ(end-1:-1:2)];
+if(mod(fftSize,2))%odd fft size
+	frequ = linspace(0,(fftSize+1)/2-1,(fftSize+1)/2) ./ fftSize .* fs;
+	frequ = [frequ(1:end) frequ(end:-1:2)];
 else%even fft size
+	frequ = fs/2 * linspace(0,1,fftSize/2+1);
 	frequ = [frequ(1:end) frequ(end-1:-1:2)];
 end
+
+%!test # even number of samples
+%! sig = [1 2 3 4];
+%! [sigFd freq] = fftAndFrequ(sig,8);
+%! assert(freq,[0,2,4,2]);
+%!test # odd number of samples
+%! sig = [1 2 3 4 5];
+%! [sigFd freq] = fftAndFrequ(sig,10);
+%! assert(freq,[0,2,4,4,2]); # reference matlab: [~,x]=specgram([1,2,3,4,5],[],10);
