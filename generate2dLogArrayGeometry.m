fd087434f44b11100d42c68f991e1de009bdf34c
Thomas Fehér <thomas.feher@yahoo.de>
2014-04-04 17:32:29 +0200

    implenent algorithm for array generation
    Taken from Book "Microphone Arrays, Signal Processing Techniques and
    Applications" (Brandstein)

--- generate2dLogArrayGeometry.m
+++ generate2dLogArrayGeometry.m
@@ -1,22 +1,29 @@
 %TODO parameters: smallest distance, arraySize; and remove branchLength
 function geometry = generate2dLogArrayGeometry(branchNum,micNumMax,branchLength
-                                              ,type,coeffA,coeffB)
-if(nargin<6)
-	coeffB = 1;
-end
-if(nargin<5)
-	switch type
+                                              ,type,coeffA,coeffB,coeffC)
+switch type
 case 'straight'
-	coeffA = 10;
+	if(nargin<5)
+		coeffA = 10;
+	end
+case 'brandstein'
+	if(nargin<7)
+		error('You have to provide 3 parameters for brandstein algorithm');
+	end
+	appertSize = coeffA;
+	freqLo = coeffB;
+	freqHi = coeffC;
 case 'spiral'
-	coeffA = 1;
-end
+	if(nargin<5)
+		coeffA = 1;
+	end
 end
 if nargin < 4
 	disp 'usage: generate2dLogArrayGeometry(branchNum,micNumMax,branchLength,type)';
 end
 
 n = 1; % number of full circles to be covert by spiral arm 
+speedOfSound = 340; % speed of sound in m/s
 
 %generate rotation matrix
 ang = 2*pi/branchNum;
@@ -31,6 +38,21 @@ case 'straight'
 	                     ,log10((1+branchLength)*coeffA)...
 	                     ,micsPerBranch)-1) / coeffA
 	           ;zeros(1,micsPerBranch)];
+case 'brandstein'
+	innerNum = floor(appertSize/2); % (1.18a) n ≤ Q/2
+	if(innerNum>micsPerBranch-1)
+		innerNum = micsPerBranch-1;
+	end
+	geometry = [0:innerNum] .* speedOfSound/(2*freqHi); % (1.18a)
+	posMax = (appertSize-1)*speedOfSound/(2*freqLo); % (1.18b) p_n < (Q-1)c/2f_L
+	for micCnt=innerNum+2:micsPerBranch % TODO vectorize % n > Q/2
+		pos = appertSize/(appertSize-1)*geometry(end); % (1.18b)
+		if(pos > posMax)
+			break
+		end
+		geometry = [geometry pos];
+	end
+	geometry = [geometry;zeros(1,numel(geometry))];
 case 'spiral'
 	for micCnt=1:micsPerBranch
 		geometry(1,micCnt) = coeffA*cos(angleFunc(micCnt,n,micsPerBranch))...
