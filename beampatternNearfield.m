e60211f740d4e7c36e3639dd34469b2939373c28
Thomas Fehér <thomas.feher@yahoo.de>
2014-04-10 16:50:52 +0200

    improve demo and tests

--- beampatternNearfield.m
+++ beampatternNearfield.m
@@ -46,15 +46,15 @@ ret = sqrt(sum((vec1-vec2).^2));
 %! targets = [-90:90];
 %! targets = targets/180*pi;
 %! targetNum = numel(targets);
-%! options.beamforming.beampattern.targets = [1e3*sin(targets);zeros(1,targetNum);1e3*cos(targets)];
+%! options.beamforming.beampattern.targets = 1e3*[sin(targets);zeros(1,targetNum);cos(targets)];
 %! startTime = cputime;
 %! for i=1:100
 %! result = beampatternNearfield(options);
 %! end
 %! endTime = cputime;
-%! subplot(2,1,1);
+%! subplot(3,1,1);
 %! plot(targets/pi*180,result.pattern);
-%! title(['cpu time:' num2str((endTime-startTime)/100)]);
+%! title(['nearfield with 1km distance, cpu time:' num2str((endTime-startTime)/100)]);
 %! options.beamforming.beampattern.phi = 0;
 %! theta = [-90:90];
 %! options.beamforming.beampattern.teta = theta;
@@ -63,23 +63,44 @@ ret = sqrt(sum((vec1-vec2).^2));
 %! result = beampattern(options);
 %! end
 %! endTime = cputime;
-%! subplot(2,1,2);
+%! subplot(3,1,2);
 %! plot(theta,result.pattern);
-%! title(['cpu time:' num2str((endTime-startTime)/100)]);
+%! title(['farfield approximation, cpu time:' num2str((endTime-startTime)/100)]);
+%! options.beamforming.beampattern.targets = 2*[sin(targets);zeros(1,targetNum);cos(targets)];
+%! result = beampatternNearfield(options);
+%! subplot(3,1,3);
+%! plot(targets/pi*180,result.pattern);
+%! title('nearfield with 2m distance');
 
-%!test # compare with farfield method
-%! options.frequNum = 10;
+%!shared options,targetNum,freqNum
+%! freqNum = 10;
+%! options.frequNum = freqNum;
 %! options.geometry = [-1 -0.5 0 0.5 1;zeros(2,5)];
-%! options.frequency = linspace(100,1000,10);
-%! options.beamforming.weights = ones(5,10)./5;
+%! options.frequency = linspace(100,1000,freqNum);
+%! options.beamforming.weights = ones(5,freqNum)./5;
 %! options.c = 340;
 %! targets = [-90:90];
 %! targets = targets/180*pi;
 %! targetNum = numel(targets);
-%! options.beamforming.beampattern.targets = [1e3*sin(targets);zeros(1,targetNum);1e3*cos(targets)];
+%! options.beamforming.beampattern.targets = 1e3*[sin(targets);zeros(1,targetNum);cos(targets)];
+
+%!test # compare with farfield method
 %! resultNear = beampatternNearfield(options);
 %! options.beamforming.beampattern.phi = 0;
 %! theta = [-90:90];
 %! options.beamforming.beampattern.teta = theta;
 %! result = beampattern(options);
-%! assert(resultNear.pattern,squeeze(result.pattern),0.01);
+%! assert(resultNear.pattern,squeeze(result.pattern),1e-4);
+
+%!test # output size
+%! result = beampatternNearfield(options);
+%! assert(size(result.pattern),[freqNum,targetNum]);
+
+%!test # symmetric array should give symmetric pattern (tests for error in Brandstein book)
+%! targets = [-90:10:90];
+%! targets = targets/180*pi;
+%! targetNum = numel(targets);
+%! options.beamforming.beampattern.targets = 2e0*[sin(targets);zeros(1,targetNum);cos(targets)];
+%! result = beampatternNearfield(options);
+%! pattern = result.pattern;
+%! assert(pattern,pattern(:,end:-1:1),1e-12);
