4d8c49a56c16512cc7a908f3b63377428eb34b7b
Thomas Fehér <thomas.feher@yahoo.de>
2014-04-15 10:57:24 +0200

    add function to calculate directivity index
    Calculation is based on microphone position and weights, not on a
    calculated beampattern. Therefore it should be more accurate and also
    much faster.

---
+++ directivityIndexFromWeights.m
@@ -0,0 +1,37 @@
+function di = directivityIndexFromWeights(weights,freqs,geometry,target,speedOfSound)
+% Calculates the directivty index from given microphone positions and weights
+% and a look direction. It uses the approach given in Brandstein "Microphone
+% Arrays - Signal Processing Techniques and Applications" equation 2.18.
+%
+% input:
+%   weights: microphone weights, size: [mic,freq]
+%   freqs: vector containing the frequencies in Hz
+%   geometry: microphone positions, [x1,x2,…;y1,y2,…;z1,z2,…]
+%   target: reference position to calculate directivity at, given as column
+%              vector of coordinates
+%   speedOfSound: speed of sound in m/s
+% output:
+%  di: directivity index in dB, size [1,freq]
+
+% wave vector for target direction
+wVec = squeeze(waveVec(geometry,freqs,target,speedOfSound));
+
+nominator = abs(sum(conj(weights) .* wVec)).^2;
+
+% coherence matrix
+cohMat = coherenceMatNearfield(geometry,freqs,'diffuse',speedOfSound);
+
+denom = sum(squeeze(mult3dArray(permute(conj(weights),[3,1,2]),cohMat)) .* weights);
+
+di = nominator ./ denom;
+
+%!test # size of output
+%! micNum = 3;
+%! freqNum = 10;
+%! weights = rand(micNum,freqNum);
+%! freqs = linspace(100,1000,freqNum);
+%! geometry = rand(3,micNum);
+%! target = rand(3,1);
+%! speedOfSound = 340;
+%! x = directivityIndexFromWeights(weights,freqs,geometry,target,speedOfSound);
+%! assert(size(x),[1,freqNum]);
