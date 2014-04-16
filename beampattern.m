a29eb6dbf2f66c9f75b71c4e056f73b8844fd1bf
Thomas Fehér <thomas.feher@yahoo.de>
2014-04-16 18:19:03 +0200

    vectorize beampattern calculation

--- beampattern.m
+++ beampattern.m
@@ -34,22 +34,50 @@ theta = theta/180*pi;
 result.teta = theta/pi*180;
 phiNum = numel(phi);
 thetaNum = numel(theta);
-pattern = zeros(micNum,frequNum,phiNum,thetaNum);
+%pattern = zeros(micNum,frequNum,phiNum,thetaNum);
 c = options.c;
+[thetaMesh phiMesh] = meshgrid(theta,phi);
+% target positions, use nearfield equation with very distant points
+targets = 1e10*[sin(thetaMesh(:)).*cos(phiMesh(:))...
+               ,sin(thetaMesh(:)).*sin(phiMesh(:))...
+               ,cos(thetaMesh(:))].';
+% vector of incoming wave from wanted direction, size: [mic,freq,targets]
+wVec = waveVec(geometry,frequency,targets,c);
+patternNoSquare = squeeze(sum(conj(W) .* wVec)); % size: [freq,targets]
+pattern = abs(patternNoSquare).^2; % size: [freq,targets]
 
-for thetaCnt=1:thetaNum
-	x = (geometry(1,:).'*sin(theta(thetaCnt))*cos(phi)).';
-	y = (geometry(2,:).'*sin(phi)*sin(theta(thetaCnt))).';
-	z = geometry(3,:)*cos(theta(thetaCnt));
-	for phiCnt=1:phiNum
-		r = exp(i*2*pi*frequency'*(x(phiCnt,:)+y(phiCnt,:)+z)/c);
-		pattern(:,:,phiCnt,thetaCnt) = W(:,:).*r.';
-	end
-end
-patternSquare = shiftdim(abs(sum(pattern)).^2);
-result.pattern = patternSquare;
-result.patternOrig = shiftdim(sum(pattern));
-result.patternSingle = pattern;
+result.pattern = pattern;
+result.patternNoSquare = patternNoSquare;
+result.patternOrig = patternNoSquare.';
+result.patternSingle = patternNoSquare;
+
+%for thetaCnt=1:thetaNum
+	%x = (geometry(1,:).'*sin(theta(thetaCnt))*cos(phi)).';
+	%y = (geometry(2,:).'*sin(phi)*sin(theta(thetaCnt))).';
+	%z = geometry(3,:)*cos(theta(thetaCnt));
+	%for phiCnt=1:phiNum
+		%r = exp(i*2*pi*frequency'*(x(phiCnt,:)+y(phiCnt,:)+z)/c);
+		%pattern(:,:,phiCnt,thetaCnt) = W(:,:).*r.';
+	%end
+%end
+%patternSquare = shiftdim(abs(sum(pattern)).^2);
+%result.pattern = patternSquare;
+%result.patternOrig = shiftdim(sum(pattern));
+%result.patternSingle = pattern;
+
+%!test # output format
+%! freqNum = 4
+%! options.frequNum = freqNum;
+%! options.geometry = [0 1715e-4;0 0;0 0];
+%! options.frequency = linspace(10,1000,freqNum);
+%! options.beamforming.weights = [1;1];
+%! phiNum = 5;
+%! thetaNum = 6;
+%! options.beamforming.beampattern.phi = linspace(-90,90,phiNum);
+%! options.beamforming.beampattern.teta = linspace(-90,90,thetaNum);
+%! options.c = 343;
+%! result = beampattern(options);
+%! assert(size(result.pattern),[freqNum,thetaNum*phiNum]);
 
 %test 2 mics 0.1715m distance -> 0.5ms delay (17e-2/340) -> makes phase shift
 %of pi/10 (test here: http://www.sengpielaudio.com/Rechner-LaufzeitPhase.htm)
@@ -63,10 +91,9 @@ result.patternSingle = pattern;
 %! options.beamforming.beampattern.teta = 90;
 %! options.c = 343;
 %! result = beampattern(options);
-%! assert(angle(result.patternOrig),pi/10/2,eps);
+%! assert(angle(result.patternOrig),-pi/10/2,1e-5);
 
-%test with more frequencies
-%!test
+%!test # test with more frequencies
 %! options.frequNum = 3;
 %! options.geometry = [0 1715e-4;0 0;0 0];
 %! options.frequency = [100 200 500];
@@ -75,10 +102,9 @@ result.patternSingle = pattern;
 %! options.beamforming.beampattern.teta = 90;
 %! options.c = 343;
 %! result = beampattern(options);
-%! assert(angle(result.patternOrig),[pi/10/2;pi/10;pi/2/2],eps);
+%! assert(angle(result.patternOrig),-1*[pi/10/2;pi/10;pi/2/2],1e-5);
 
-%test with more phi angles
-%!test
+%!#test # test with more phi angles
 %! options.frequNum = 3;
 %! options.geometry = [0 1715e-4;0 0;0 0];
 %! options.frequency = [100 200 500];
